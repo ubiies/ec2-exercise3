0632e4936032034be22d3faa7fd7b50eb5d86d3a
ubiies <alal55al9@naver.com>
2023-05-23 14:48:14 +0900

    first commit

---
+++ src/test/java/com/example/ec2exercise3/Ec2Exercise3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec2exercise3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2Exercise3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
